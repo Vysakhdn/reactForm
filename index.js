1b2d2314c76cfc2c3b41365f708e51546feb378a
Vysakhdn <vysakhdn123@gmail.com>
2023-08-14 20:51:49 +0530

    form

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyForm from './MyForm';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MyForm />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
